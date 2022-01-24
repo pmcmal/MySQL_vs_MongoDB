c590112ffdaa2f0fd0c9906cf782dad6e0bf65e1
Paweł Malec <89246706+pmcmal@users.noreply.github.com>
2022-01-24 16:42:12 +0100

    Delete models directory

--- models/temp.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('../index_mongodb');
-const TempSchema = new mongoose.Schema({
-    temp: String,
-});
-
-const Temp = mongoose.model('temp', TempSchema);
-module.exports = Temp;
