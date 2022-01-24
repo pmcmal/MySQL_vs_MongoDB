3ea53e25e2c4dc8183a9c557e10f55e4ff4ec906
Paweł Malec <89246706+pmcmal@users.noreply.github.com>
2022-01-24 16:40:35 +0100

    Delete util.js

--- util.js
+++
@@ -1,9 +0,0 @@
-module.exports.calcTime = (initialTime, finalTime) => {
-  if (finalTime === undefined) {
-    finalTime = Date.now();
-  }
-  return ((finalTime - initialTime) / 1000) + ' s';
-}
-module.exports.format = (map) => {
-  return `Count: ${map.count} Time ${map.time} `;
-}
